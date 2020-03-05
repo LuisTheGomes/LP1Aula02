d6694f5f79f0c473828c58b20878191500be1b03
Luís Gomes <afonsofgomes@gmail.com>
2020-03-05 14:56:11 +0000

    Added Volume

---
+++ Cilindro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Altura: ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Raio: ");
+
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            double V = 3.1415926*Math.Pow(r,2)*a ; 
+
+            Console.WriteLine("Volume: {0}",V);
+
+        }
+    }
+}

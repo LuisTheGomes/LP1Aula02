3251eb35cf3bf4e4433a983a31efc4565afdf75c
Luís Gomes <afonsofgomes@gmail.com>
2020-03-05 14:30:53 +0000

    First commit

---
+++ IncDec/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0;
+
+            Console.WriteLine(a++);
+
+            Console.WriteLine(++a);
+        }
+    }
+}
